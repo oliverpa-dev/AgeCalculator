f372ce14f2c7c16e82dd6db43db04bb65306b8e4
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2023-10-03 19:40:11 +0200

    Add: Relevant DOM nodes

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Input elements
+const dayInput = document.getElementById("input-day");
+const monthInput = document.getElementById("input-month");
+const yearInput = document.getElementById("input-year");
+
+// Button
+const calcBtn = document.getElementById("btn-calc");
+
+// Output elements
+const dayOutput = document.getElementById("output-day");
+const monthOutput = document.getElementById("output-month");
+const yearOutput = document.getElementById("output-year");
